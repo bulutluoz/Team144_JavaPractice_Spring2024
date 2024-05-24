deef4622b2f87e8bee12c5eeb464f5f59c487a43
bulutluoz <abbulutluoz@gmail.com>
2024-05-24 09:50:06 +0200

    day04 sonu

--- src/day04_StringManipulations_ForLoop/C03_NullPointer.java
+++ src/day04_StringManipulations_ForLoop/C03_NullPointer.java
@@ -5,36 +5,41 @@ public class C03_NullPointer {
 
     public static void main(String[] args) {
 
-        // Asagidaki kod calistirilirsa her satirda ne yazdirir?
-
         /*
+            Null Pointer : bir deger degil ISARETCIDIR
 
-        String a = "";
-
-        String b ;
-
-        String c = null;
-
-        String d = "Java";
-
-        System.out.println(a);
-
-        System.out.println(b);
-
-        System.out.println(c);
-
-        System.out.println(a + d);
-
-        System.out.println(b + d);
-
-        System.out.println(c + d);
-
-        System.out.println(a.length());
-
-        System.out.println(b.length());
-
-        System.out.println(c.length());
+            b ve c deger atanmayan variable'lardir
+            java deger atanmayan b'yi kullanmaniza HIC BIR SEKILDE izin vermez
+            ama null olarak ISARETLENEN variable'lari SADECE yazdirmamiza izin verir
 
          */
+
+        // Asagidaki kod calistirilirsa her satirda ne yazdirir?
+
+                String a = "";          // degeri var
+
+                String b ;              // degeri yok
+
+                String c = null;        // degeri yok
+
+                String d = "Tava";      // degeri var
+
+                System.out.println(a);  // konsolda bos bir satir gorurur
+
+                //System.out.println(b);// Compile Time Error
+                                        // degeri olmayan bir variable KULLANILAMAZ
+                System.out.println(c);  // null yazdirir
+
+        //        System.out.println(a + d);
+        //
+        //        System.out.println(b + d);
+        //
+        //        System.out.println(c + d);
+        //
+        //        System.out.println(a.length());
+        //
+        //        System.out.println(b.length());
+        //
+        //        System.out.println(c.length());
     }
 }
